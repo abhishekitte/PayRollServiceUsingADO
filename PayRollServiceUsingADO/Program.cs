489a65813404f426f2e9d52903cacfcfa1ffeff3
ABHI <abhishekitte11@gmail.com>
2021-10-26 23:16:19 +0530

    [ABHISHEK] Added a code to perform two table using transaction in the databas

--- PayRollServiceUsingADO/Program.cs
+++ PayRollServiceUsingADO/Program.cs
@@ -10,20 +10,7 @@ namespace PayrollServicesUsingADO
     {
         static void Main(string[] args)
         {
-            EmployeeRepository repository = new EmployeeRepository();
-           
-            EmployeeModel model = new EmployeeModel();
-            //model.EmployeeName = "Abhishek";
-            //model.BasicPay = 950000;
-            ////repository.UpdateSalaryUsingStoredProcedure(model);
-            //model.StartDate = DateTime.Today;
-            //model.Gender = "M";
-            ////repository.PerformAggregateFunctions(model);
-            //model.PhoneNumber =9967320888;
-            //model.Address = "Mumbai";
-            //model.Department = "Sales";
-            //model.TaxablePay = 5000;
-            repository.AddEmployee(model);
+
         }
     }
 }
