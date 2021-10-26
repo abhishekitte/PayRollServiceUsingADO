6fc24f70e589a0417ad86554df9ffb1d6e6c37cf
ABHI <abhishekitte11@gmail.com>
2021-10-26 22:34:58 +0530

    [ABHISHEK] Added a code to retrieve all details database

--- PayRollServiceUsingADO/Program.cs
+++ PayRollServiceUsingADO/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PayRollServicesUsingADO
+namespace PayrollServicesUsingADO
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Creating a object for employeerepository
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
         }
     }
 }
